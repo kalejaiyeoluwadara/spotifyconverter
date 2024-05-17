21db29f16e74865ee2ea144076e25dc61976c3d4
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-05-17 10:21:54 +0100

    workks

--- src/PLaylist.jsx
+++ src/PLaylist.jsx
@@ -133,14 +133,12 @@ const YoutubePlaylistCreator = () => {
           onFailure={handleLoginFailure}
         />
         {isLoading && <p>Loading...</p>}
-        {accessToken && (
           <button
             onClick={fetchVideos}
             className="mt-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
           >
             Create Playlist
           </button>
-        )}
         {playlistLink && (
           <div className="mt-4">
             <p>Playlist Link:</p>
